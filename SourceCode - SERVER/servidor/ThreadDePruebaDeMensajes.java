cfc0434cb81257dc1216ec50f7af5a65c8c6591d
Fernandi Nicolas Vazquez <fernando_vazquez_3@hotmail.com>
2015-07-20 12:32:54 -0300

    Update ThreadDePruebaDeMensajes.java

--- SourceCode - SERVER/servidor/ThreadDePruebaDeMensajes.java
+++ SourceCode - SERVER/servidor/ThreadDePruebaDeMensajes.java
@@ -4,9 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Es una clase temporal para mandar mensajes a un cliente por consola. Se utiliza para las prueba de los mensajes del cliente
+ **/
+ 
 public class ThreadDePruebaDeMensajes extends Thread {
 	private Servidor servidor;
 	
+	/**
+	 * En el constructor se le asigna el servidor al que hace la regerencia. el servidos contiene la lista de los clientes activos a mandar el mensaje de prueva.
+	 **/
 	public ThreadDePruebaDeMensajes(Servidor servidor) {
 		this.servidor = servidor;
 	}
